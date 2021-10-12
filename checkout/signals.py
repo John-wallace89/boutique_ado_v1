74b6877e3b6b546709882b2ec2440a30ead0a04e
John-wallace89 <john.wallace@heineken.co.uk>
2021-10-12 14:39:23 +0000

    added checkout views and payments.

--- checkout/signals.py
+++ checkout/signals.py
@@ -3,10 +3,11 @@ from django.dispatch import receiver
 
 from .models import OrderLineItem
 
-@receiver(post_save, sender=OrderLineItem()
+
+@receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
     """
-    Update order total on Lineitem update/create
+    Update order total on lineitem update/create
     """
     instance.order.update_total()
 
